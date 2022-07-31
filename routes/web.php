8745f28622e34bc0a3fc1369bd448b246ce9a996
Robby Gustian <robbygustian11@gmail.com>
2022-08-01 00:00:32 +0700

    clear file dan clean code

--- routes/web.php
+++ routes/web.php
@@ -23,33 +23,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return redirect()->intended('/login');
 });
 Route::get('/reload-captcha', [UserController::class, 'reloadCaptcha']);
-Route::get('/templates',function(){
+Route::get('/templates', function () {
     return view('template');
 });
-Route::group(['middleware'=>'guest'],function(){
-    Route::get('/login',[UserController::class,'loginView'])->name('login');
-    Route::post('/login',[UserController::class,'login']);
-    Route::post('/submission',[ParticipantController::class,'submission']);
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [UserController::class, 'loginView'])->name('login');
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/submission', [ParticipantController::class, 'submission']);
 });
 
-Route::group(['middleware'=>'auth'],function(){
-    Route::get('/dashboard',[UserController::class,'dashboard']);
-    Route::get('/logout',[UserController::class,'logout']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [UserController::class, 'dashboard']);
+    Route::get('/logout', [UserController::class, 'logout']);
 });
 
-Route::group(['middleware'=>'isHC'],function(){
-    Route::get('/data-pengajuan',[ParticipantController::class,'index']);
+Route::group(['middleware' => 'isHC'], function () {
+    Route::get('/data-pengajuan', [ParticipantController::class, 'index']);
 
-    Route::get('/data-peserta', [ParticipantController::class, 'getParticipant']); 
-    Route::get('/data-peserta/{id}',[ParticipantController::class,'edit'])->name('participant.edit');
-    Route::put('/data-peserta/{id}',[ParticipantController::class,'update'])->name('participant.update');
-    Route::put('/data-peserta/{id}/accept',[ParticipantController::class,'acceptSubmission']);
-    Route::delete('/data-peserta/{id}/reject',[ParticipantController::class,'rejectSubmission']);
-    
+    Route::get('/data-peserta', [ParticipantController::class, 'getParticipant']);
+    Route::get('/data-peserta/{id}', [ParticipantController::class, 'edit'])->name('participant.edit');
+    Route::put('/data-peserta/{id}', [ParticipantController::class, 'update'])->name('participant.update');
+    Route::put('/data-peserta/{id}/accept', [ParticipantController::class, 'acceptSubmission']);
+    Route::delete('/data-peserta/{id}/reject', [ParticipantController::class, 'rejectSubmission']);
+
     Route::resource('/data-pembimbing', MentorController::class, [
         'names' => [
             'index' => 'mentor.index',
@@ -59,7 +60,7 @@ Route::group(['middleware'=>'isHC'],function(){
             'update' => 'mentor.update',
             'destroy' => 'mentor.destroy'
         ]
-    ]);    
+    ]);
 
     Route::resource('/data-divisi', DivisionController::class, [
         'names' => [
@@ -70,7 +71,7 @@ Route::group(['middleware'=>'isHC'],function(){
             'update' => 'division.update',
             'destroy' => 'division.destroy'
         ]
-    ]);    
+    ]);
 
     Route::resource('/data-instansi', InstituteController::class, [
         'names' => [
@@ -81,7 +82,7 @@ Route::group(['middleware'=>'isHC'],function(){
             'update' => 'division.update',
             'destroy' => 'division.destroy'
         ]
-    ]);  
+    ]);
 
     Route::resource('/data-jurusan', MajorController::class, [
         'names' => [
@@ -92,20 +93,20 @@ Route::group(['middleware'=>'isHC'],function(){
             'update' => 'division.update',
             'destroy' => 'division.destroy'
         ]
-    ]);  
+    ]);
 });
 
-Route::group(['middleware'=>'isMentor'],function(){
-    Route::get('/list-peserta', [ParticipantController::class, 'getParticipantByMentor']);   
-    Route::get('/list-tugas/create', [TaskController::class, 'create']);   
-    Route::post('/list-tugas/store', [TaskController::class, 'store']);   
-    Route::get('/list-tugas', [TaskController::class, 'getTaskByMentor']);   
-    Route::get('/list-presensi', [AttendanceController::class, 'getAttendanceByMentor']);   
+Route::group(['middleware' => 'isMentor'], function () {
+    Route::get('/list-peserta', [ParticipantController::class, 'getParticipantByMentor']);
+    Route::get('/list-tugas/create', [TaskController::class, 'create']);
+    Route::post('/list-tugas/store', [TaskController::class, 'store']);
+    Route::get('/list-tugas', [TaskController::class, 'getTaskByMentor']);
+    Route::get('/list-presensi', [AttendanceController::class, 'getAttendanceByMentor']);
 });
 
-Route::group(['middleware'=>'isParticipant'],function(){
-    Route::post('/clock-in',[AttendanceController::class,'clockIn']);
-    Route::put('/clock-out',[AttendanceController::class,'clockOut']);
+Route::group(['middleware' => 'isParticipant'], function () {
+    Route::post('/clock-in', [AttendanceController::class, 'clockIn']);
+    Route::put('/clock-out', [AttendanceController::class, 'clockOut']);
 });
 
 Route::get('/presensi', function () {
